629ba875f69564ebf242847bef484d6b8a97d6aa
shong2430 <134196918+shong2430@users.noreply.github.com>
2023-09-18 04:19:26 +0800

    Updates

--- .storybook/config.js
+++
@@ -1,4 +0,0 @@
-import { loadStories } from "./load-stories";
-import { configure } from "@storybook/react";
-
-configure(loadStories, module);
--- src/pages/home/routes.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import {Route} from "react-router";
-import home from './index';
-
-export default [
-    <Route key={"/home"} exact path={'home'} component={home}/>
-];
